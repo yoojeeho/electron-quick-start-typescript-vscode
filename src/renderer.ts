0ad19f55ccb2c97bbfbdc3aa113bff7dde381cbd
yoojeeho <dbwlgh628@gmail.com>
2019-02-03 03:17:40 +0900

    Init

---
+++ src/renderer.ts
@@ -0,0 +1,11 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+setTimeout(() => {
+    console.log("Hello, World!");
+}, 5000);
+
+window.onclick = () => {
+    console.log("Hello, Click!");
+}
